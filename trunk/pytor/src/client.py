6d0aa5867931e747387e429911b51953744d97fb
msk <msk@18a754c8-05c4-11df-87ec-d30288882566>
2010-04-24 17:40:59 +0000

    typo in the license header

--- trunk/pytor/src/client.py
+++ trunk/pytor/src/client.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # 
-# Copyright (c) 2010 GhostHunter
+# Copyright (c) 2010 Xavier Garcia
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
