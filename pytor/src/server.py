452e10a7fc20b2c0b1e8ea9a0adb7e33cd544d69
msk <msk@18a754c8-05c4-11df-87ec-d30288882566>
2010-04-24 17:40:59 +0000

    typo in the license header

--- pytor/src/server.py
+++ pytor/src/server.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 # 
-# Copyright (c) 2010 GhostHunter
+# Copyright (c) 2010 Xavier Garcia
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
