fb1465a7f142a255c39bb1ad3857d369ed235b09
msk <msk@18a754c8-05c4-11df-87ec-d30288882566>
2010-10-03 17:35:12 +0000

    Fixed import problems

--- fimap/plugins/reversehttp/pytor/src/gen_rsa_keys.py
+++ fimap/plugins/reversehttp/pytor/src/gen_rsa_keys.py
@@ -1,7 +1,19 @@
 #! /usr/bin/python
-import rsa
+from pytor import *
+import os
 
 # this script generates the public/private keys
 # that can be used with the reverse shell
 
-print  "public=%s\nprivate=%s" % rsa.gen_pubpriv_keys(1024)
+print "Generating the key pair. It may take some time"
+pub_key, priv_key= rsa.gen_pubpriv_keys(1024)
+print "Writing the keys"
+
+pub_fd=open('./pub.txt','w')
+pub_fd.write(str(pub_key))
+pub_fd.close()
+
+
+priv_fd=open('./priv.txt','w')
+priv_fd.write(str(priv_key))
+priv_fd.close()
